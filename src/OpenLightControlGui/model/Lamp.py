2dd2a09a67faf8739eebfba78cd9c578d8d838b6
tobi08151405 <tobias.teichmann@gmx.at>
2021-08-02 16:26:35 +0200

    Lamp uses now Mode, not Fixture

--- src/OpenLightControlGui/model/Lamp.py
+++ src/OpenLightControlGui/model/Lamp.py
@@ -3,16 +3,17 @@ from numbers import Number
 
 from .Address import Address
 from OpenLightControlGui.fixture_model.Fixture import Fixture
-from OpenLightControlGui.fixture_model.Capability import Capability
+from OpenLightControlGui.fixture_model.Mode import Mode
+from OpenLightControlGui.fixture_model.AbstractChannel import AbstractChannel
 
 class Lamp():
     _address: 'list[Address]'
-    _fixture: Fixture
+    _mode: Mode
     _num: Number
 
-    def __init__(self, num: Number, fixture: Fixture, address: Optional[Union[Address, Iterable[Address]]] = None) -> None:
+    def __init__(self, num: Number, mode: Mode, address: Optional[Union[Address, Iterable[Address]]] = None) -> None:
         self._num = num
-        self._fixture = fixture
+        self._mode = mode
         if address:
             if not isinstance(address, Iterable):
                 self._address = [address]
@@ -24,11 +25,14 @@ class Lamp():
     def get_num(self) -> Number:
         return self._num
     
-    def get_fixture(self) -> Fixture:
-        return self._fixture
+    def get_mode(self) -> Mode:
+        return self._mode
     
-    def get_capabilities(self) -> 'list[Capability]':
-        return self.fixture.capabilities
+    def get_fixture(self) -> Fixture:
+        return self.mode.fixture
+    
+    def get_channels(self) -> 'list[AbstractChannel]':
+        return self.mode.channels
 
     def get_address(self) -> 'list[Address]':
         return self._address
@@ -43,19 +47,20 @@ class Lamp():
         return len(self._address) > 0
     
     number: Number = property(get_num)
+    mode: Mode = property(get_mode)
     fixture: Fixture = property(get_fixture)
-    capabilities: 'list[Capability]' = property(get_capabilities)
+    channels: 'list[AbstractChannel]' = property(get_channels)
     address: 'list[Address]' = property(get_address, set_address)
     hasAddress: bool = property(get_hasAddress)
 
     def __eq__(self, o: 'Lamp') -> bool:
-        return isinstance(o, Lamp) and self.address == o.address and self.fixture == o.fixture
+        return isinstance(o, Lamp) and self.address == o.address and self.mode == o.mode
 
     def __str__(self) -> str:
         if self.hasAddress:
-            return f"{self.number}-{self.fixture.name} at {self.address}"
+            return f"{self.number}-{self.fixture.name}-{self.mode.name} at {self.address}"
         else:
-            return f"{self.number}-{self.fixture.name}"
+            return f"{self.number}-{self.fixture.name}-{self.mode.name}"
     
     def __repr__(self) -> str:
         return self.__str__()
