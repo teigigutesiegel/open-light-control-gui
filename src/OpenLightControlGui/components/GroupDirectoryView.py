4f3530429623b84a64bc712b0e0c029a9c53ab58
tobi08151405 <tobias.teichmann@gmx.at>
2021-08-20 22:18:19 +0200

    added placeholder init var

--- src/OpenLightControlGui/components/GroupDirectoryView.py
+++ src/OpenLightControlGui/components/GroupDirectoryView.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import pyqtSignal
 
 if TYPE_CHECKING:
     from PyQt5.QtWidgets import QWidget
+    from PyQt5.QtGui import QColor
 
 
 class GroupDirectoryView(AbstractDirectoryView):
@@ -14,20 +15,23 @@ class GroupDirectoryView(AbstractDirectoryView):
 
     group_selected = pyqtSignal(int, Group)
 
-    def __init__(self, parent: Optional['QWidget'] = None) -> None:
-        super().__init__(parent=parent)
+    def __init__(self, parent: Optional['QWidget'] = None, placeholder: int = 1000) -> None:
+        super().__init__(parent=parent, placeholder=placeholder)
         self._groups = {}
 
         self._title = "Group"
         self.setWindowTitle(f"{self._title} Directory")
     
-    def addGroup(self, num: int, group: Group) -> None:
+    def addGroup(self, num: int, group: Group, color: Optional['QColor'] = None) -> None:
         self._groups[num] = group
         if not group.name:
             group.name = f"Group {num}"
-        item = self.ViewTile(num, group.name)
-        item.clicked.connect(lambda x, num = num: self._handle_TileClick(num))
+        item = self.ViewTile(num, group.name, color)
+        item.selected.connect(lambda num: self._handle_TileClick(num))
         self.setItem(num, item)
+    
+    def getGroup(self, num: int) -> Optional[Group]:
+        return self._groups.get(num)
 
     def _handle_TileClick(self, num: int) -> None:
         self.group_selected.emit(num, self._groups.get(num))
