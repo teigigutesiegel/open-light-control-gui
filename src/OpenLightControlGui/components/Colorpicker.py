a7001522b3f0614f07cab9fd3609291509aed50f
tobi08151405 <tobias.teichmann@gmx.at>
2021-12-28 13:03:31 +0100

    rewrite of Colorpicker

--- src/OpenLightControlGui/components/Colorpicker.py
+++ src/OpenLightControlGui/components/Colorpicker.py
@@ -1,29 +1,17 @@
-from typing import Optional
 from PyQt5.QtWidgets import QWidget, QApplication, QSizePolicy, QHBoxLayout, QSlider
-from PyQt5.QtCore import Qt, pyqtSignal, QPointF, QRect
+from PyQt5.QtCore import Qt, pyqtSignal, QPointF, QRect, QLineF
 from PyQt5.QtGui import QColor, QResizeEvent, QPaintEvent, QPainter, QConicalGradient, QRadialGradient, QMouseEvent
 
-import numpy as np
-import sys
-
-from OpenLightControlGui import AspectLayout
-
-
 class ColorCircle(QWidget):
-    px: float
-    py: float
-    h: float
-    s: float
-    v: float
     currentColorChanged = pyqtSignal(QColor)
 
-    def __init__(self, parent: Optional[QWidget] = None, startupcolor: 'list[int]' = [255, 255, 255], margin: int = 10) -> None:
+    def __init__(self, parent=None, startupcolor: list = [255, 255, 255], margin=10) -> None:
         super().__init__(parent=parent)
         self.radius = 0
         self.selected_color = QColor(
             startupcolor[0], startupcolor[1], startupcolor[2], 1)
-        self.px = 0.5
-        self.py = 0.5
+        self.x = 0.5
+        self.y = 0.5
         self.h = self.selected_color.hueF()
         self.s = self.selected_color.saturationF()
         self.v = self.selected_color.valueF()
@@ -34,10 +22,13 @@ class ColorCircle(QWidget):
         qsp.setHeightForWidth(True)
         self.setSizePolicy(qsp)
 
-    def resizeEvent(self, a0: QResizeEvent) -> None:
-        self.radius = min([self.width()/2, self.height()/2])
+    def resizeEvent(self, ev: QResizeEvent) -> None:
+        size = min(self.width(), self.height()) - self.margin * 2
+        self.radius = size / 2
+        self.square = QRect(0, 0, size, size)
+        self.square.moveCenter(self.rect().center())
 
-    def paintEvent(self, a0: QPaintEvent) -> None:
+    def paintEvent(self, ev: QPaintEvent) -> None:
         center = QPointF(self.width()/2, self.height()/2)
         p = QPainter(self)
         p.setViewport(self.margin, self.margin, self.width() -
@@ -53,57 +44,41 @@ class ColorCircle(QWidget):
 
         p.setPen(Qt.transparent)
         p.setBrush(hsv_grad)
-        p.drawEllipse(self.rect())
+        p.drawEllipse(self.square)
         p.setBrush(val_grad)
-        p.drawEllipse(self.rect())
+        p.drawEllipse(self.square)
 
-        p.setViewport(QRect(0, 0, self.width(), self.height()))
         p.setPen(Qt.black)
         p.setBrush(self.selected_color)
-        x = self.width()*self.px
-        y = self.height()*self.py
-        p.drawEllipse(self.line_circle_inter(
-            x, y, self.width()/2, self.height()/2, self.radius), 10, 10)
+        line = QLineF.fromPolar(self.radius * self.s, 360 * self.h + 90)
+        line.translate(self.rect().center())
+        p.drawEllipse(line.p2(), 10, 10)
 
     def recalc(self) -> None:
         self.selected_color.setHsvF(self.h, self.s, self.v)
         self.currentColorChanged.emit(self.selected_color)
         self.repaint()
 
-    def line_circle_inter(self, x: float, y: float, m_x: float, m_y: float, r: float) -> QPointF:
-        m = np.array([m_x, m_y])
-        p = np.array([x, y])
-        d = p - m
-        dist = np.linalg.norm(d)
-        vec = d/dist
-        c = m+vec*r-vec*self.margin
-        return QPointF(c[0], c[1]) if dist >= r else QPointF(x, y)
-
-    def map_color(self, x: int, y: int) -> 'tuple[float, float, float]':
-        h = (np.arctan2(x-self.radius, y-self.radius)+np.pi)/(2.*np.pi)
-        s = np.sqrt(np.power(x-self.radius, 2) +
-                    np.power(y-self.radius, 2))/self.radius
-        v = self.v
-        if s > 1.0:
-            s = 1.0
-        return h, s, v
+    def map_color(self, x: int, y: int) -> QColor:
+        line = QLineF(QPointF(self.rect().center()), QPointF(x, y))
+        s = min(1.0, line.length() / self.radius)
+        h = (line.angle() - 90) / 360 % 1.
+        return h, s, self.v
 
     def processMouseEvent(self, ev: QMouseEvent) -> None:
-        x = ev.x()
-        y = ev.y()
-        if (ev.button() == Qt.RightButton):
-            x = self.width() / 2
-            y = self.height() / 2
-        self.h, self.s, self.v = self.map_color(x, y)
-        self.px = x / self.width()
-        self.py = y / self.height()
+        if ev.button() == Qt.MouseButton.RightButton:
+            self.h, self.s, self.v = 0, 0, 1
+        else:
+            self.h, self.s, self.v = self.map_color(ev.x(), ev.y())
+        self.x = ev.x() / self.width()
+        self.y = ev.y() / self.height()
         self.recalc()
 
-    def mouseMoveEvent(self, a0: QMouseEvent) -> None:
-        self.processMouseEvent(a0)
+    def mouseMoveEvent(self, ev: QMouseEvent) -> None:
+        self.processMouseEvent(ev)
 
-    def mousePressEvent(self, a0: QMouseEvent) -> None:
-        self.processMouseEvent(a0)
+    def mousePressEvent(self, ev: QMouseEvent) -> None:
+        self.processMouseEvent(ev)
 
     def setHue(self, hue: float) -> None:
         if 0 <= hue <= 1:
@@ -148,18 +123,15 @@ class ColorCircle(QWidget):
 class ColorCircleDialog(QWidget):
     currentColorChanged = pyqtSignal(QColor)
 
-    def __init__(self, parent: Optional[QWidget] = None, width: int = 500, startupcolor: 'list[int]' = [255, 255, 255]) -> None:
+    def __init__(self, parent=None, width: int = 500, startupcolor: list = [255, 255, 255]) -> None:
         super().__init__(parent=parent)
         self.resize(width, width)
 
-        lay = AspectLayout(1)
+        mainlay = QHBoxLayout()
         wid = ColorCircle(self, startupcolor=startupcolor)
         wid.currentColorChanged.connect(
             lambda x: self.currentColorChanged.emit(x))
-        lay.addWidget(wid)
-
-        mainlay = QHBoxLayout()
-        mainlay.addLayout(lay)
+        mainlay.addWidget(wid)
         fader = QSlider()
         fader.setMinimum(0)
         fader.setMaximum(511)
@@ -171,6 +143,7 @@ class ColorCircleDialog(QWidget):
 
 
 if __name__ == '__main__':
+    import sys
     app = QApplication(sys.argv)
     
     window = ColorCircleDialog()
